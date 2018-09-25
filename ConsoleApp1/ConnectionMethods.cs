249e8609b9ef4e8afa4b80871de10d1e48259533
Vijay Adhithyan Mohan <vijay@thewebco.co.nz>
2018-09-26 08:03:40 +1200

    Initial CRM Commit

---
+++ ConsoleApp1/ConnectionMethods.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class ConnectionMethods
+    {
+        //private Guid _accountId;
+        private static IOrganizationService _orgService;
+
+        public void Run(String connectionString)
+        {
+            try
+            {
+                // Connect to the CRM web service using a connection string.
+                ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+                CrmServiceClient conn = new CrmServiceClient(connectionString);
+
+                // Cast the proxy client to the IOrganizationService interface.
+                _orgService = (IOrganizationService)conn.OrganizationWebProxyClient != null ? (IOrganizationService)conn.OrganizationWebProxyClient : (IOrganizationService)conn.OrganizationServiceProxy;
+
+                // Retrieve the version of Microsoft Dynamics CRM.
+                RetrieveVersionRequest versionRequest = new RetrieveVersionRequest();
+                RetrieveVersionResponse versionResponse =
+                    (RetrieveVersionResponse)_orgService.Execute(versionRequest);
+                Console.WriteLine("Microsoft Dynamics CRM version {0}.", versionResponse.Version);
+                
+            }
+
+            // Catch any service fault exceptions that Microsoft Dynamics CRM throws.
+            catch (FaultException<Microsoft.Xrm.Sdk.OrganizationServiceFault>)
+            {
+                // You can handle an exception here or pass it back to the calling method.
+                throw;
+            }
+        }
+        public static String GetServiceConfiguration()
+        {
+            // Get available connection strings from app.config.
+            int count = ConfigurationManager.ConnectionStrings.Count;
+            List<KeyValuePair<String, String>> filteredConnectionStrings =
+                new List<KeyValuePair<String, String>>();
+
+            for (int a = 0; a < count; a++)
+            {
+                if (isValidConnectionString(ConfigurationManager.ConnectionStrings[a].ConnectionString))
+                    filteredConnectionStrings.Add
+                        (new KeyValuePair<string, string>
+                            (ConfigurationManager.ConnectionStrings[a].Name,
+                            ConfigurationManager.ConnectionStrings[a].ConnectionString));
+            }
+
+            // No valid connections strings found. Write out and error message.
+            if (filteredConnectionStrings.Count == 0)
+            {
+                Console.WriteLine("An app.config file containing at least one valid Microsoft Dynamics CRM " +
+                    "connection string configuration must exist in the run-time folder.");
+                Console.WriteLine("\nThere are several commented out example connection strings in " +
+                    "the provided app.config file. Uncomment one of them and modify the string according " +
+                    "to your Microsoft Dynamics CRM installation. Then re-run the sample.");
+                return null;
+            }
+
+            // If one valid connection string is found, use that.
+            if (filteredConnectionStrings.Count == 1)
+            {
+                return filteredConnectionStrings[0].Value;
+            }
+
+            // If more than one valid connection string is found, let the user decide which to use.
+            if (filteredConnectionStrings.Count > 1)
+            {
+                Console.WriteLine("The following connections are available:");
+                Console.WriteLine("------------------------------------------------");
+
+                for (int i = 0; i < filteredConnectionStrings.Count; i++)
+                {
+                    Console.Write("\n({0}) {1}\t",
+                    i + 1, filteredConnectionStrings[i].Key);
+                }
+
+                Console.WriteLine();
+
+                Console.Write("\nType the number of the connection to use (1-{0}) [{0}] : ",
+                    filteredConnectionStrings.Count);
+                String input = Console.ReadLine();
+                int configNumber;
+                if (input == String.Empty) input = filteredConnectionStrings.Count.ToString();
+                if (!Int32.TryParse(input, out configNumber) || configNumber > count ||
+                    configNumber == 0)
+                {
+                    Console.WriteLine("Option not valid.");
+                    return null;
+                }
+
+                return filteredConnectionStrings[configNumber - 1].Value;
+
+            }
+            return null;
+
+        }
+        private static Boolean isValidConnectionString(String connectionString)
+        {
+            // At a minimum, a connection string must contain one of these arguments.
+            if (connectionString.Contains("Url=") ||
+                connectionString.Contains("Server=") ||
+                connectionString.Contains("ServiceUri="))
+                return true;
+
+            return false;
+        }
+    }
+}
